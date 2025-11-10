e19d8c37c21b4404a2c24a0c7715b2afa412b476
Pranesh-D-code <praneshcommon@gmail.com>
2025-11-10 06:44:21 +0530

    Delete IdeaProjects/Mini Main 2 directory
    delete

--- IdeaProjects/Mini Main 2/src/main/java/com/example/mini_main_2/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.mini_main_2;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
